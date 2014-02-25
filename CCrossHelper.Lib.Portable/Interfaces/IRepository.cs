3ba8a2d0e3f4c061fd90369f403e33400a4d5688
Philippe Matray <phmatray@outlook.com>
2014-02-25 01:10:51 +0100

    Ajout d'une interface IRepository à la PCL

---
+++ CCrossHelper.Lib.Portable/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CCrossHelper.Lib.Portable.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+        T Read(int id);
+        void Update(T entity);
+        void Delete(T entity);
+        IEnumerable<T> SelectAll();
+    }
+}
