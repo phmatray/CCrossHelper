01057417799932b040b9011f7e2f33f62c47ed73
Philippe Matray <philippe.matray@thesmartcompany.com>
2014-02-26 10:24:17 +0100

    Ajout d'une méthode Shuffle sur IList sans recréer la liste. Ajout d'une méthode pour générer des mots de passe.

---
+++ CCrossHelper.Lib.Portable/Extensions/ListExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CCrossHelper.Lib.Portable.Extensions
+{
+    public static class ListExtension
+    {
+        /// <summary>
+        ///     Shuffles the specified source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        public static void Shuffle<T>(this IList<T> source)
+        {
+            // http://stackoverflow.com/questions/273313/
+
+            var rnd = new Random();
+            int n = source.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                T value = source[k];
+                source[k] = source[n];
+                source[n] = value;
+            }
+        }
+    }
+}
---
+++ CCrossHelper.Lib.Portable/Tools/PasswordFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CCrossHelper.Lib.Portable.Extensions;
+
+namespace CCrossHelper.Lib.Portable.Tools
+{
+    public class PasswordFactory
+    {
+        private readonly Random _rnd;
+
+        public PasswordFactory()
+        {
+            _rnd = new Random();
+        }
+
+        public string Generate(int passwordLength = 8)
+        {
+            if (passwordLength < 4)
+                throw new ArgumentOutOfRangeException("passwordLength", "cannot be smaller than 4");
+
+            const string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string lowers = "abcdefghijklmnopqrstuvwxyz";
+            const string numbers = "0123456789";
+            const string specials = "~!@#$%^&*().<>?{}[]";
+
+            var authorisedCases = new List<string> { uppers, lowers, numbers, specials };
+
+
+            // first 4 chars
+            var password = new List<char>
+            {
+                uppers[_rnd.Next(uppers.Length)],
+                lowers[_rnd.Next(lowers.Length)],
+                numbers[_rnd.Next(numbers.Length)],
+                specials[_rnd.Next(specials.Length)]
+            };
+
+            // last chars
+            int remainingChars = passwordLength - password.Count;
+            for (int i = 0; i < remainingChars; i++)
+            {
+                string caseCollection = authorisedCases[_rnd.Next(authorisedCases.Count)];
+                password.Add(caseCollection[_rnd.Next(caseCollection.Length)]);
+            }
+
+            // shake password
+            password.Shuffle();
+
+            // convert to string and return
+            return password.Aggregate(string.Empty, (current, c) => current + c);
+        }
+    }
+}
