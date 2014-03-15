e1e4f07b629cea6de222a2650ca784204182b7f2
Philippe Matray <phmatray@outlook.com>
2014-03-15 05:51:32 +0100

    + Ajout d'un StringFormatConverter

---
+++ CCrossHelper.Lib.Store/Converters/StringFormatConverter.cs
@@ -0,0 +1,32 @@
+﻿/* Author:
+ * Diederik Krols
+ * 
+ * Link: 
+ * http://blogs.u2u.be/diederik/post/2012/03/19/A-StringFormat-converter-for-Windows-8-Metro.aspx
+ * 
+ * Date:
+ * 2012-03-19
+ */
+
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace CCrossHelper.Lib.Store.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // No format provided.
+            if (parameter == null)
+                return value;
+
+            return String.Format((String)parameter, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
